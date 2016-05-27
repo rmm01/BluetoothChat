ba3d660676ee6a654c58bbda5c1ed7ba1af0e61d
Ricky Martinez <crosscountry68@gmail.com>
2016-05-26 21:39:56 -0700

    added broadcast receiver that listens for changes in bluetooth. TextView now displays the state of bluetooth Menu items are enabled and disabled based on bluetooth state

--- app/src/main/java/com/yckir/bluetoothchat/MainActivity.java
+++ app/src/main/java/com/yckir/bluetoothchat/MainActivity.java
@@ -1,6 +1,10 @@
 package com.yckir.bluetoothchat;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,25 +13,63 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_ENABLE_BT = 1;
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,
+                        BluetoothAdapter.ERROR);
+                switch (state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        mTextView.setText(R.string.bluetooth_disabled_message);
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        mTextView.setText(R.string.bluetooth_turning_off_message);
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        mTextView.setText(R.string.bluetooth_enabled_message);
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        mTextView.setText(R.string.bluetooth_turning_on_message);
+                        break;
+                }
+            }
+        }
+    };
+
     private BluetoothAdapter mBlueToothAdapter;
     private boolean mBlueToothSupported;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.message);
 
         mBlueToothAdapter = BluetoothAdapter.getDefaultAdapter();
         mBlueToothSupported = mBlueToothAdapter != null;
+
         if(!mBlueToothSupported){
-            setContentView(R.layout.no_bluetooth);
+            mTextView.setText(R.string.no_bluetooth);
             return;
         }
 
+        if(mBlueToothAdapter.isEnabled()){
+            mTextView.setText(R.string.bluetooth_enabled_message);
+        }else{
+            mTextView.setText(R.string.bluetooth_disabled_message);
+        }
 
-        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -40,7 +82,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Register for broadcasts on BluetoothAdapter state change
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(mReceiver, filter);
+    }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        unregisterReceiver(mReceiver);
     }
 
     @Override
@@ -60,10 +111,30 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_create) {
+            if(!mBlueToothAdapter.isEnabled())
+                return true;
+            //bluetooth is enabled, launch activity to create a chat room
+        }
+        if (id == R.id.action_join) {
+            if(!mBlueToothAdapter.isEnabled())
+                return true;
+            //bluetooth is enabled, launch activity to join a chat room
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        //if bluetooth is disabled then disable the buttons that require bluetooth
+        if(!mBlueToothAdapter.isEnabled()){
+            menu.getItem(0).setEnabled(false);
+            menu.getItem(1).setEnabled(false);
+        }else{
+            menu.getItem(0).setEnabled(true);
+            menu.getItem(1).setEnabled(true);
+        }
+        return super.onPrepareOptionsMenu(menu);
     }
 }
