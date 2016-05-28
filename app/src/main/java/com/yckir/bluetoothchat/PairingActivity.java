ffb012840ffe973500afe62383145b52029b01c7
Ricky Martinez <crosscountry68@gmail.com>
2016-05-28 14:31:36 -0700

    PairingActivity      - now has a receiver for being notified for changes in bluetooth state      - now listens for changes in bluetooth switch state.      - closes the activity if no bluetooth is available      - recievers are registered and unregistered in onstart and onstop      - onStart checks to make sure that activity is in correct state if activity was removed from foreground      - the state of the activity is always accurate with that of the bluetooth, even if bluetooth is changed from status bar or when it is restored from background

--- app/src/main/java/com/yckir/bluetoothchat/PairingActivity.java
+++ app/src/main/java/com/yckir/bluetoothchat/PairingActivity.java
@@ -1,20 +1,47 @@
 package com.yckir.bluetoothchat;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SwitchCompat;
+import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.pnikosis.materialishprogress.ProgressWheel;
 
-public class PairingActivity extends AppCompatActivity {
+public class PairingActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, BluetoothStatusReceiver.BlueToothStatusListener {
 
     private TextView mBlueToothName;
-    private SwitchCompat mEnableBluetooth;
+    private TextView mDiscoverable;
+    private TextView mFindDevices;
+    private SwitchCompat mBluetoothSwitch;
     private ProgressWheel mDiscoverableWheel;
-    private ProgressWheel mDiscoveringWheel;
+    private ProgressWheel mFindDevicesWheel;
+    private BluetoothAdapter mBluetoothAdapter;
+
+    private BluetoothStatusReceiver mBTStatusReceiver = null;
+
+    private void enableBluetoothFields(boolean enabled){
+
+        //if one of the fields is enabled and the parameter is the same, do nothing since state is already correct
+        if(enabled == mDiscoverable.isEnabled())
+            return;
+
+        mDiscoverable.setEnabled(enabled);
+        mFindDevices.setEnabled(enabled);
+        if(!enabled) {
+            mDiscoverableWheel.stopSpinning();
+            mFindDevicesWheel.stopSpinning();
+        }else{
+            mDiscoverableWheel.spin();
+            mFindDevicesWheel.spin();
+        }
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,15 +49,109 @@ public class PairingActivity extends AppCompatActivity {
         setContentView(R.layout.pairing_layout);
 
         mBlueToothName = (TextView)findViewById(R.id.bluetooth_name);
-        mEnableBluetooth = (SwitchCompat)findViewById(R.id.enable_blue_tooth);
+        mDiscoverable = (TextView)findViewById(R.id.enable_discovery_label);
+        mFindDevices = (TextView)findViewById(R.id.find_devices_prompt);
+        mBluetoothSwitch = (SwitchCompat)findViewById(R.id.enable_blue_tooth);
         mDiscoverableWheel = (ProgressWheel)findViewById(R.id.enable_discovery);
-        mDiscoveringWheel = (ProgressWheel)findViewById(R.id.find_devices);
+        mFindDevicesWheel = (ProgressWheel)findViewById(R.id.find_devices);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        //set default state
+        mBlueToothName.setText( mBluetoothAdapter.getName() );
+        mBluetoothSwitch.setChecked(mBluetoothAdapter.isEnabled());
+        enableBluetoothFields(mBluetoothAdapter.isEnabled());
+
+        mBluetoothSwitch.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mBluetoothSwitch.setChecked(mBluetoothAdapter.isEnabled());
+
+        if(mBTStatusReceiver == null) {
+            mBTStatusReceiver = new BluetoothStatusReceiver();
+            mBTStatusReceiver.setListener(this);
+            registerReceiver(mBTStatusReceiver, BluetoothStatusReceiver.getIntentFilter());
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mBTStatusReceiver != null) {
+            unregisterReceiver(mBTStatusReceiver);
+            mBTStatusReceiver = null;
+        }
     }
 
     public void makeDiscoverable(View view){
-
+        Log.v("PAIR_ACTIVITY", "makeDiscoverable");
     }
 
     public void findDevices(View view) {
+        Log.v("PAIR_ACTIVITY", "findDevices");
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        Log.v("PAIR_ACTIVITY", "bluetoothSwitch");
+
+        if(isChecked){
+            if(mBluetoothAdapter.isEnabled()){
+                //bluetooth is already on, make sure fields are enabled
+                enableBluetoothFields(true);
+            }else{
+                mBluetoothAdapter.enable();
+                //don't enable fields since bluetooth takes time to turn on
+            }
+        }else{
+            if(mBluetoothAdapter.isEnabled()){
+                mBluetoothAdapter.disable();
+                enableBluetoothFields(false);
+            }else{
+                //bluetooth is already disabled, make sure fields are off
+                enableBluetoothFields(false);
+            }
+        }
+    }
+
+    @Override
+    public void bluetoothOff() {
+        Toast.makeText(this, "Bluetooth is off", Toast.LENGTH_SHORT).show();
+        mBluetoothSwitch.setChecked(false);
+        mBluetoothSwitch.setEnabled(true);
+        enableBluetoothFields(false);
+    }
+
+    @Override
+    public void bluetoothOn() {
+        Toast.makeText(this, "Bluetooth is on", Toast.LENGTH_SHORT).show();
+        mBluetoothSwitch.setChecked(true);
+        mBluetoothSwitch.setEnabled(true);
+        enableBluetoothFields(true);
+    }
+
+    @Override
+    public void bluetoothTurningOff() {
+        //Toast.makeText(this, "Bluetooth is turning off", Toast.LENGTH_SHORT).show();
+        mBluetoothSwitch.setChecked(false);
+        mBluetoothSwitch.setEnabled(true);
+        enableBluetoothFields(false);
+    }
+
+    @Override
+    public void bluetoothTurningOn() {
+        Toast.makeText(this, "Bluetooth is turning on", Toast.LENGTH_SHORT).show();
+        mBluetoothSwitch.setChecked(true);
+        mBluetoothSwitch.setEnabled(false);
+        enableBluetoothFields(false);
     }
 }
