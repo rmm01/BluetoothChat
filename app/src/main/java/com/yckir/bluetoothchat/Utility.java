c1686f2c40e2d04bece562e27a1e2b6b1ff7d2f4
Ricky Martinez <crosscountry68@gmail.com>
2016-06-02 10:36:01 -0700

    Created a service that  receives a bluetooth socket from a bound client and can then be used to send messages using that socket. Utility constants have been added for the new service
    BluetoothWriteService
         - has a WriteBinder that is used to give service a bluetooth socket.
         - clients must bind with the flag BIND_AUTO_CREATE so that the service does not stop itself while a client is bound.
         - Uses constants in utility class for actions, extra parameters, and message types.
         - sendDisplayText sends an intent to service to send the text to the other device.

--- app/src/main/java/com/yckir/bluetoothchat/Utility.java
+++ app/src/main/java/com/yckir/bluetoothchat/Utility.java
@@ -3,13 +3,23 @@ package com.yckir.bluetoothchat;
 import java.util.UUID;
 
 public class Utility {
+    //number of bytes int the message
+    public static final int LENGTH_OF_SEND_ID = 4;
 
+    //ids of the types of messages being sent
+    public static final String ID_SEND_DISPLAY_TEXT = "0000";
+    public static final String ID_SEND_CLOSE        = "0001";
+
+    //send actions for intents
+    public static final String ACTION_SEND_DISPLAY_TEXT = "ACTION_SEND_DISPLAY_TEXT";
+
+    //extra fields for send intents
+    public static final String EXTRA_SEND_DISPLAY_TEXT  = "EXTRA_SEND_TEXT";
+    public static final String EXTRA_SEND_ID  = "EXTRA_SEND_ID";
 
     public static final String SDP_NAME = "BluetoothChat";
     public static UUID getBTChatUUID(){
         byte[] b = "ajsvcrgcdfg".getBytes();
         return UUID.nameUUIDFromBytes(b);
     }
-
-
 }
---
+++ app/src/main/java/com/yckir/bluetoothchat/services/BluetoothWriteService.java
@@ -0,0 +1,150 @@
+package com.yckir.bluetoothchat.services;
+
+
+import android.app.IntentService;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.yckir.bluetoothchat.Utility;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Class that handles write requests using a bluetooth socket. Uses IntentService that handles request
+ * one at a time on a separate thread.
+ */
+public class BluetoothWriteService extends IntentService {
+
+    private static final String TAG = "WriteService";
+    private BluetoothSocket mSocket;
+    private OutputStream mOutStream;
+    private WriteBinder mBinder;
+
+    public BluetoothWriteService() {
+        super(TAG);
+        mBinder = new WriteBinder();
+    }
+
+    /**
+     * handles merging the parameters into a message that will be sent.
+     * @param displayText the text that will be sent
+     */
+    private void handleActionSendMessage(String displayText){
+        String data = Utility.ID_SEND_DISPLAY_TEXT + displayText;
+        byte[] bytes = data.getBytes();
+        try {
+            mOutStream.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.v(TAG, "Could not write message : " + data);
+        }
+    }
+
+    /**
+     * Send text that will be displayed on the connected devices.
+     * Sends an intent to the service that requests the message be sent using the bluetooth socket.
+     *
+     * @param context app context
+     * @param message the message to be sent using socket.
+     */
+    public static void sendDisplayText(Context context, String message){
+        Log.v(TAG, "startActionWrite");
+
+        Intent intent = new Intent(context, BluetoothWriteService.class);
+        intent.setAction(Utility.ACTION_SEND_DISPLAY_TEXT);
+        intent.putExtra(Utility.EXTRA_SEND_DISPLAY_TEXT, message);
+
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if(mSocket == null){
+            Log.v(TAG, "onHandleIntent no socket");
+            return;
+        }
+        Log.v(TAG, "onHandleIntent");
+
+        if(intent == null){
+            Log.v(TAG, "null intent");
+            return;
+        }
+
+        String action = intent.getAction();
+
+        switch (action){
+            case Utility.ACTION_SEND_DISPLAY_TEXT:
+                String message = intent.getStringExtra(Utility.EXTRA_SEND_DISPLAY_TEXT);
+                handleActionSendMessage(message);
+                break;
+            default:
+                Log.v(TAG,"unknown action " + action);
+                break;
+        }
+
+
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.v(TAG, "onBind");
+        return mBinder;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.v(TAG, "onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.v(TAG, "onUnbind");
+        return false;
+    }
+
+
+    /**
+     * Binder that is used to give the service a reference to the bluetooth socket to write to.
+     */
+    public class WriteBinder extends Binder {
+        /**
+         * @param socket the bluetooth socket used to communicate with another device
+         * @return true false if an OutputStream could not be retrieved from the socket.
+         */
+        public boolean setSocket(@NonNull BluetoothSocket socket){
+
+            Log.v(TAG, "socket set");
+
+            OutputStream tmpOut;
+
+            try {
+                tmpOut = socket.getOutputStream();
+            }catch (IOException e){
+                Log.e(TAG, "Could not get output stream form socket");
+                return false;
+            }
+
+            if(mOutStream != null){
+                try {
+                    mOutStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.w(TAG, "could not close the socket before switching");
+                }
+            }
+
+            mSocket = socket;
+            mOutStream = tmpOut;
+            return true;
+        }
+    }
+}
