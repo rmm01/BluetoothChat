1694a4f6c787d6fbf6e7544708b8e4125bd93c81
Ricky Martinez <crosscountry68@gmail.com>
2016-07-04 15:35:09 -0700

    recycler view is now height match parent

--- app/src/main/java/com/yckir/bluetoothchat/recycler/RecyclerDividerDecoration.java
+++ app/src/main/java/com/yckir/bluetoothchat/recycler/RecyclerDividerDecoration.java
@@ -30,7 +30,7 @@ public class RecyclerDividerDecoration extends RecyclerView.ItemDecoration{
         int right = parent.getWidth() - parent.getPaddingRight();
 
         int childCount = parent.getChildCount();
-        for (int i = 0; i < childCount; i++) {
+        for (int i = 0; i < childCount - 1; i++) {
             View child = parent.getChildAt(i);
 
             RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
